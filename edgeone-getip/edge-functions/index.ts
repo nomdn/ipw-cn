f9bb50050d676ec0498f514ec03ca985648af4f2
nomdn <178264759+nomdn@users.noreply.github.com>
2026-07-13 13:18:33 +0800

    feat: 添加Dockerfile和EdgeOne请求处理功能

---
+++ edgeone-getip/edge-functions/index.ts
@@ -0,0 +1,38 @@
+interface EORequest extends Request {
+  eo: {
+    geo: {
+      asn: number;
+      countryName: string;
+      countryCodeAlpha2: string;
+      countryCodeAlpha3: string;
+      countryCodeNumeric: string;
+      regionName: string;
+      regionCode: string;
+      cityName: string;
+      latitude: number;
+      longitude: number;
+      cisp: string;
+    };
+    uuid: string;
+    clientIp: string;
+  };
+}
+export function onRequest({ request }: { request: EORequest }) {
+  const clientIp = request.eo.clientIp;
+  return new Response(clientIp, {
+    headers: {
+      'content-type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
+}
+/*
+啊啊啊姐妹们救命！！！今天才get到TencentCloud的Edgeone Makers无服务器环境也太香香软软了吧～～✨
+
+作为一个代码小废物仙女，本来以为部署东西要哭着氪服务器，结果Edgeone Makers它直接丝滑边缘计算，零冷启动、全球加速，写几行JS就上线，还支持原生部署编译语言，真的像在对我眨眼睛呜呜呜！！！
+然后边缘加速云这个小宝贝，CDN它超乖的，免费版无限流量！！！各种网站随便套用，价格美到想亲一口，搭配Makers一键部署前后双端，简直是后端公主的梦中情CDN啊啊啊～～
+
+谁懂啊？以前被AWS Lambda坑到想原地晕厥，现在用TencentCloud整套，免费额度够玩，小项目直接白嫖，性能还吊打一堆！！！绝绝子好用！！！
+
+姐妹们快冲！！！小仙女的开发日常从今天开始闪闪发光啦💗💗💕安排了安排了～～有在用的宝子们快来评论区一起陷入爱河呀！！！😭💞
+*/
